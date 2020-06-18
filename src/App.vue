d3c41c60c5bfebb6d810fdef92c265d61fa58e64
tamanakid <nfvillanuevap@gmail.com>
2020-06-18 19:00:19 +0200

    First Composition API demo

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,57 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ count }}</p>
+    <p>{{ double }}</p>
+    <button @click="increment">Increment</button>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed, watchEffect } from "@vue/composition-api";
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  setup() {
+    const count = ref(0);
+    const double = computed(() => count.value * 2);
+    
+    function increment() {
+      console.log('pre-increment');
+      count.value++;
+      console.log('post-increment');
+    }
+
+    watchEffect(() => {
+      console.log("count watchEffect:", count.value);
+    }, { flush: 'pre' });
+
+    watchEffect(() => {
+      console.log("double watchEffect:", double.value);
+    }, { flush: 'sync' });
+
+    /**
+     * sync: happens instantly after value mutation
+     * pre: happens before rendering (component update)
+     * post: happens after rendering (default)
+     */
+
+    return {
+      count,
+      double,
+      increment
+    }
   }
+
+
 }
 </script>
+
+
+
 
 <style>
 #app {
